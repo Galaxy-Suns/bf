6355dccf1895c150d479c323981414354612f3c6
galaxy <me@galaxies.top>
2025-08-15 16:07:34 +0800

    Re-origanize src/bf.cpp

--- src/bf.cpp
+++ src/bf.cpp
@@ -10,18 +10,13 @@
 const int MEM_SIZE = 30000;
 const int MATCH_BUFFER_SIZE = 1000;
 
-std::string remove_comment(const std::string &origin) {
-    std::istringstream iss{origin};
-    std::ostringstream oss;
-    std::string line;
-    while(std::getline(iss, line)) {
-        for (int i = 0; i < line.size(); i++) {
-            if (line[i] == '/') break;
-            if (std::string{".,+-<>[]"}.find(line[i]) != std::string::npos) oss << line[i];
-        }
-    }
-    return oss.str();
-}
+std::string valid_code(const std::string &origin); 
+void output(uint8_t c, bool output_number, bool enter); 
+uint8_t input(bool input_number); 
+int loop_left(std::vector<int> &left, uint8_t c, int i, const std::string &bf_code); 
+int find_right_loop(const std::string &bf_code, int i); 
+int loop_right(std::vector<int> &left, uint8_t c, int i); 
+void visualize(const std::string &bf_code, uint8_t *mem, int visual, int vi, int index); 
 
 void bf_handler(
     std::string bf_code_origin, 
@@ -31,11 +26,11 @@ void bf_handler(
     bool enter,
     int max_step
 ) {
-    std::string bf_code = remove_comment(bf_code_origin);
+    std::string bf_code = valid_code(bf_code_origin);
     uint8_t mem[MEM_SIZE]{0};
     int index = 0;
-    int left_order = 0;
     std::vector<int> left;
+
     for(int i = 0; i < bf_code.size() && max_step != 0; i++, max_step--) {
         int vi = i;
         char c = bf_code[i];
@@ -53,74 +48,109 @@ void bf_handler(
             index++;
             break;
         case '.':
-            if (output_number) std::cout << (int) mem[index] << std::flush;
-            else std::cout << (char) mem[index] << std::flush;
-            if (enter) std::cout << std::endl;
+            output(mem[index], output_number, enter);
             break;
         case ',':
-            std::cout << ">>> ";
-            if (input_number) {
-                int tmp;
-                std::cin >> tmp;
-                mem[index] = (char) tmp;
-            } else {
-                char tmp;
-                std::cin >> tmp;
-                mem[index] = tmp;
-            }
+            mem[index] = input(input_number);
             break;
         case '[':
-            left.push_back(i);
-            if (mem[index] == 0) {
-                std::vector<int> stack;
-                for (int ti = i + 1; ti < bf_code.size(); ti++) {
-                    if (bf_code[ti] == '[') stack.push_back(ti);
-                    else if(bf_code[ti] == ']') {
-                        if (stack.empty()) {
-                            i = ti - 1;
-                            break;
-                        }
-                        else stack.pop_back();
-                    }
-                }
-            }
+            i = loop_left(left, mem[index], i, bf_code);
             break;
         case ']':
-            if (mem[index] != 0) {
-                i = left.back() - 1;
-            }
-            left.pop_back();
+            i = loop_right(left, mem[index], i);
             break;
-        default: continue;
         }
-        if (visual) {
-            for (int j = 0; j < bf_code.size(); j++) {
-                if (vi == j) std::cout << termcolor::green;
-                std::cout << bf_code[j];
-                if (vi == j) std::cout << termcolor::reset;
-            }
-            std::cout << std::endl;
-            for (int i = 0; i < visual; i++) {
-                if (index == i) std::cout << termcolor::green;
-                std::cout << (int) mem[i] << '\t';
-                if (index == i) std::cout << termcolor::reset;
-            }
-            std::cout << std::endl;
-            std::cout << termcolor::blue;
-            std::ostringstream oss;
-            bool printFlag = false;
-            for (int i = 0; i < visual; i++) {
-                char c = (char) mem[i];
-                if(std::isprint(c)) {
-                    oss<< (char) mem[i] << '\t';
-                    printFlag = true;
-                }
-                else oss << ' ' << '\t';
-            }
-            if (printFlag) std::cout << oss.str() << std::endl;
-            std::cout << termcolor::reset;
-            std::cout << "----" << std::endl;
-        }
-        
+        if (visual) visualize(bf_code, mem, visual, vi, index);
     }
+}
+
+void output(uint8_t c, bool output_number, bool enter) {
+    if (output_number) std::cout << (int) c << std::flush;
+    else std::cout << (char) c << std::flush;
+    if (enter) std::cout << std::endl;
+}
+
+uint8_t input(bool input_number) { 
+    std::cout << ">>> ";
+    if (input_number) {
+        int tmp;
+        std::cin >> tmp;
+        return tmp;
+    } else {
+        char tmp;
+        std::cin >> tmp;
+        return tmp;
+    }
+}
+
+int loop_left(std::vector<int> &left, uint8_t c, int i, const std::string &bf_code) {
+    left.push_back(i);
+    if (c == 0) {
+        i = find_right_loop(bf_code, i);
+    }
+    return i;
+}
+
+int find_right_loop(const std::string &bf_code, int i) {
+    std::vector<int> stack;
+    for (int ti = i + 1; ti < bf_code.size(); ti++) {
+        if (bf_code[ti] == '[') stack.push_back(ti);
+        else if(bf_code[ti] == ']') {
+            if (stack.empty()) {
+                return ti;
+            }
+            else stack.pop_back();
+        }
+    }
+    return -1;
+}
+
+int loop_right(std::vector<int> &left, uint8_t c, int i) {
+    if (c != 0) {
+        i = left.back() - 1;
+    }
+    left.pop_back();
+    return i;
+}
+
+void visualize(const std::string &bf_code, uint8_t *mem, int visual, int vi, int index) {
+    for (int j = 0; j < bf_code.size(); j++) {
+        if (vi == j) std::cout << termcolor::green;
+        std::cout << bf_code[j];
+        if (vi == j) std::cout << termcolor::reset;
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < visual; i++) {
+        if (index == i) std::cout << termcolor::green;
+        std::cout << (int) mem[i] << '\t';
+        if (index == i) std::cout << termcolor::reset;
+    }
+    std::cout << std::endl;
+    std::cout << termcolor::blue;
+    std::ostringstream oss;
+    bool printFlag = false;
+    for (int i = 0; i < visual; i++) {
+        char c = (char) mem[i];
+        if(std::isprint(c)) {
+            oss<< (char) mem[i] << '\t';
+            printFlag = true;
+        }
+        else oss << ' ' << '\t';
+    }
+    if (printFlag) std::cout << oss.str() << std::endl;
+    std::cout << termcolor::reset;
+    std::cout << "----" << std::endl;
+}
+
+std::string valid_code(const std::string &origin) {
+    std::istringstream iss{origin};
+    std::ostringstream oss;
+    std::string line;
+    while(std::getline(iss, line)) {
+        for (int i = 0; i < line.size(); i++) {
+            if (line[i] == '/') break;
+            if (std::string{".,+-<>[]"}.find(line[i]) != std::string::npos) oss << line[i];
+        }
+    }
+    return oss.str();
 }
